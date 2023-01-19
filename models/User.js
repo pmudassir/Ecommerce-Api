f23ea106dc71bfcba85fbda59529cbcadf3feb9f
pmudassir <pmuddasir@gmail.com>
2023-01-19 20:29:18 +0530

    Changes made in User model

--- models/User.js
+++ models/User.js
@@ -8,6 +8,7 @@ const UserSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    img: {type: String}
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", UserSchema);
